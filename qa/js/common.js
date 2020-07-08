015ea80fd6e11b3d50eb9d4a50b0da5db0ec326b
wawacm <36990488+wawacm@users.noreply.github.com>
2020-07-08 22:31:50 +0800

    Add files via upload

--- qa/js/common.js
+++ qa/js/common.js
@@ -1,274 +1,149 @@
-var popusertimer = null;
-var query = '?';
-$(document).ready(function() {
-    //头部header浮动层
-    $(".ismore a").hover(function() {
-        $(".tuser-more-list").hide();
-        $(this).parent().next("div").show();
-        $(this).parent().next("div").hover(function() {
-        }, function() {
-            $(this).hide();
-        });
-    });
+var userAgent = navigator.userAgent.toLowerCase();
+var is_opera = userAgent.indexOf('opera') != -1 && opera.version();
+var is_moz = (navigator.product == 'Gecko') && userAgent.substr(userAgent.indexOf('firefox') + 8, 3);
+var is_ie = (userAgent.indexOf('msie') != -1 && !is_opera) && userAgent.substr(userAgent.indexOf('msie') + 5, 3);
 
-    //搜索框底层border
-    $(window).bind("scroll", function() {
-        var e = document.documentElement.scrollTop || document.body.scrollTop;
-        e > 20 ? $(".js-fixed").addClass("fixed-theme") : $(".js-fixed").removeClass("fixed-theme");
-    });
-
-
-
-    //搜索提问按钮
-    $("#search_btn").click(function() {
-        document.searchform.action = g_site_url + '' + query + 'question/search/2' + g_suffix;
-        document.searchform.submit();
-    });
-    $("#ask_btn").click(function() {
-        document.searchform.action = g_site_url + '' + query + 'question/add' + g_suffix;
-        document.searchform.submit();
-    });
-
-    //分类选择
-    $("#category1").change(function() {
-        fillcategory(category2, $("#category1 option:selected").val(), "category2");
-        $("#jiantou1").show();
-        $("#category2").show();
-    });
-    $("#category2").change(function() {
-        fillcategory(category3, $("#category2 option:selected").val(), "category3");
-        $("#jiantou2").show();
-        $("#category3").show();
-    });
-    $("#changecategory").click(function() {
-        if (!$(this).hasClass("btn-disabled-1"))
-            $("#catedialog").dialog("open");
-
-    });
-
-    //usercard关闭
-    $("#usercard").hover(function() {
-        if (popusertimer) {
-            clearTimeout(popusertimer);
-        }
-        $("#usercard").show();
-    }, function() {
-        if (popusertimer) {
-            clearTimeout(popusertimer);
-        }
-        popusertimer = setTimeout(function() {
-            $("#usercard").hide();
-        }, 300);
-    });
-    load_message();
-});
-function bytes(str) {
-    var len = 0;
-    for (var i = 0; i < str.length; i++) {
-        if (str.charCodeAt(i) > 127) {
-            len++;
-        }
-        len++;
-    }
-    return len;
-}
-//问题分类选择函数
-function initcategory(category1) {
-    var selectedcid1 = $("#selectcid1").val();
-    $("#category1").html('');
-    for (var i = 0; i < category1.length; i++) {
-        var selected = '';
-        if (selectedcid1 === category1[i][0]) {
-            selected = ' selected';
-        }
-        $("#category1").append("<option value='" + category1[i][0] + "' " + selected + ">" + category1[i][1] + "</option>");
-    }
-    $("#catedialog").dialog({
-        autoOpen: false,
-        width: 480,
-        modal: true,
-        resizable: false
-    });
-}
-function fillcategory(category2, value1, cateid) {
-    var optionhtml = '<option value="0">不选择</option>';
-    var selectedcid = 0;
-    if (cateid === "category2") {
-        selectedcid = $("#selectcid2").val();
-    } else if (cateid === "category3") {
-        selectedcid = $("#selectcid3").val();
-    }
-    $("#" + cateid).html("");
-    for (var i = 0; i < category2.length; i++) {
-        if (value1 === category2[i][0]) {
-            var selected = '';
-            if (selectedcid === category2[i][1]) {
-                selected = ' selected';
-                $("#" + cateid).show();
-            }
-            optionhtml += "<option value='" + category2[i][1] + "' " + selected + ">" + category2[i][2] + "</option>";
-        }
-    }
-    $("#" + cateid).html(optionhtml);
+function $(id) {
+	return document.getElementById(id);
 }
 
-//验证码
-function updatecode() {
-    var img = g_site_url + "index.php" + query + "user/code/" + Math.random();
-    $('#verifycode').attr("src", img);
+Array.prototype.push = function(value) {
+	this[this.length] = value;
+	return this.length;
 }
 
-//验证码检测
-function check_code() {
-    var code = $.trim($('#code').val());
-    if ($.trim(code) == '') {
-        $('#codetip').html("验证码错误");
-        $('#codetip').attr('class', 'input_error');
-        return false;
-    }
-    $.ajax({
-        type: "GET",
-        async: false,
-        cache: false,
-        url: g_site_url + "index.php" + query + "user/ajaxcode/" + code,
-        success: function(flag) {
-            if (1 == flag) {
-                $('#codetip').html("&nbsp;");
-                $('#codetip').attr('class', 'input_ok');
-                return true;
-            } else {
-                $('#codetip').html("验证码错误");
-                $('#codetip').attr('class', 'input_error');
-                return false;
-            }
-
-        }
-    });
-}
-//usercard弹出层
-function pop_user_on(popobj, uid, type) {
-    var myalign = "left-27 bottom-30";
-    if (type == 'text') {
-        myalign = "left-21 bottom-10";
-    } else if (type == 'image_active') {
-        myalign = "left-40 bottom-43";
-    } else if (type == 'image_follow') {
-        myalign = "left-10 bottom-20";
-    }
-    if (popusertimer) {
-        clearTimeout(popusertimer);
-    }
-  
-    popusertimer = setTimeout(function() {
-        $("#usercard").show();
-        $("#usercard").position({
-            my: myalign,
-            of: popobj
-        });
-    }, 300);
-    $("#usercard").load(g_site_url + "index.php" + query + "user/ajaxuserinfo/" + uid);
-}
-function pop_user_out() {
-    if (popusertimer) {
-        clearTimeout(popusertimer);
-    }
-    popusertimer = setTimeout(function() {
-        $("#usercard").hide();
-    }, 300);
+function getcookie(name) {
+	var cookie_start = document.cookie.indexOf(name);
+	var cookie_end = document.cookie.indexOf(";", cookie_start);
+	return cookie_start == -1 ? '' : unescape(document.cookie.substring(cookie_start + name.length + 1, (cookie_end > cookie_start ? cookie_end : document.cookie.length)));
 }
 
-/*用户登陆*/
-function login() {
-    $("#poplogin").remove();
-    $("body").append('<div id="poplogin" title="欢迎登陆' + g_site_name + '"></div>');
-    $("#poplogin").load(g_site_url + "index.php?user/ajaxpoplogin");
-    $("#poplogin").dialog({
-        width: 520,
-        modal: true,
-        resizable: false,
-        position: {my: "bottom-60"}
-    });
+function setcookie(cookieName, cookieValue, seconds, path, domain, secure) {
+	seconds = seconds ? seconds : 8400000;
+	var expires = new Date();
+	expires.setTime(expires.getTime() + seconds);
+	document.cookie = escape(cookieName) + '=' + escape(cookieValue)
+		+ (expires ? '; expires=' + expires.toGMTString() : '')
+		+ (path ? '; path=' + path : '/')
+		+ (domain ? '; domain=' + domain : '')
+		+ (secure ? '; secure' : '');
 }
 
-function load_message() {
-    if (g_uid == 0) {
-        return false;
-    }
-    
-    $.post(g_site_url + "index.php?user/ajaxloadmessage", function(msg) {
-    	console.log(msg);
-        if (msg.msg_personal != 0) {
-            $("#mymessage_personal").html(msg.msg_personal);
-            $("#mymessage_personal").show();
-            $("#mymessage .ismore a").removeClass("msg-null");
-        }
-        if (msg.msg_system != 0) {
-            $("#mymessage_system").html(msg.msg_system);
-            $("#mymessage_system").show();
-            $("#mymessage .ismore a").removeClass("msg-null");
-        }
-        if (msg.message_recommand != 0) {
-            $("#mymessage_recommend").html(msg.message_recommand);
-            $("#mymessage_recommend").show();
-            $("#mymessage .ismore a").removeClass("msg-null");
-        }
-    }, "json");
+function _attachEvent(obj, evt, func) {
+	if(obj.addEventListener) {
+		obj.addEventListener(evt, func, false);
+	} else if(obj.attachEvent) {
+		obj.attachEvent("on" + evt, func);
+	}
 }
 
-/*删除回答*/
-function delete_answer(aid, qid) {
-    if (confirm('确定删除问题？该操作不可返回！') === true) {
-        document.location.href = g_site_url + '' + query + 'question/deleteanswer/' + aid + '/' + qid + g_suffix;
-    }
-}
-/*关注问题*/
-function attentto_question(qid) {
-    if (g_uid == 0) {
-        login();
-    }
-    $.post(g_site_url + "index.php?question/attentto", {qid: qid}, function(msg) {
-        if (msg == 'ok') {
-            if ($("#attenttoquestion").hasClass("button_attention")) {
-                $("#attenttoquestion").removeClass("button_attention");
-                $("#attenttoquestion").addClass("button_followed");
-                $("#attenttoquestion").val("取消关注");
-            } else {
-                $("#attenttoquestion").removeClass("button_followed");
-                $("#attenttoquestion").addClass("button_attention");
-                $("#attenttoquestion").val("关注");
-            }
-        }
-    });
-}
-/*关注用户*/
-function attentto_user(uid) {
-    if (g_uid == 0) {
-        login();
-    }
-    $.post(g_site_url + "index.php?user/attentto", {uid: uid}, function(msg) {
-        if (msg == 'ok') {
-            if ($("#attenttouser_"+uid).hasClass("button_attention")) {
-                $("#attenttouser_"+uid).removeClass("button_attention");
-                $("#attenttouser_"+uid).addClass("button_followed");
-                $("#attenttouser_"+uid).val("取消关注");
-            } else {
-                $("#attenttouser_"+uid).removeClass("button_followed");
-                $("#attenttouser_"+uid).addClass("button_attention");
-                $("#attenttouser_"+uid).val("关注");
-            }
-        }
-    });
+function _cancelBubble(e, returnValue) {
+	if(!e) return ;
+	if(is_ie) {
+		if(!returnValue) e.returnValue = false;
+		e.cancelBubble = true;
+	} else {
+		e.stopPropagation();
+		if(!returnValue) e.preventDefault();
+	}
 }
 
-function checkall(checkname) {
-    var chkall = $("#chkall:checked").val();
-    if (chkall && (chkall === 'chkall')) {
-        $("input[name^='" + checkname + "']").each(function() {
-            $(this).prop("checked", "checked");
-        });
-    } else {
-        $("input[name^='" + checkname + "']").each(function() {
-            $(this).removeProp("checked");
-        });
-    }
+function checkall(name) {
+	var e = is_ie ? event : checkall.caller.arguments[0];
+	obj = is_ie ? e.srcElement : e.target;
+	var arr = document.getElementsByName(name);
+	var k = arr.length;
+	for(var i=0; i<k; i++) {
+		arr[i].checked = obj.checked;
+	}
+}
+
+function getposition(obj) {
+	var r = new Array();
+	r['x'] = obj.offsetLeft;
+	r['y'] = obj.offsetTop;
+	while(obj = obj.offsetParent) {
+		r['x'] += obj.offsetLeft;
+		r['y'] += obj.offsetTop;
+	}
+	return r;
+}
+
+function addMouseEvent(obj){
+	var checkbox,atr,ath,i;
+	atr=obj.getElementsByTagName("tr");
+	for(i=0;i<atr.length;i++){
+		atr[i].onclick=function(){
+			ath=this.getElementsByTagName("th");
+			checkbox=this.getElementsByTagName("input")[0];
+			if(!ath.length && checkbox.getAttribute("type")=="checkbox"){
+				if(this.className!="currenttr"){
+					this.className="currenttr";
+					checkbox.checked=true;
+				}else{
+					this.className="";
+					checkbox.checked=false;
+				}
+			}
+		}
+	}
+}
+
+// editor.js
+if(is_ie) document.documentElement.addBehavior("#default#userdata");
+
+function setdata(key, value){
+	if(is_ie){
+		document.documentElement.load(key);
+		document.documentElement.setAttribute("value", value);
+		document.documentElement.save(key);
+		return  document.documentElement.getAttribute("value");
+	} else {
+		sessionStorage.setItem(key,value);
+	}
+}
+
+function getdata(key){
+	if(is_ie){
+		document.documentElement.load(key);
+		return document.documentElement.getAttribute("value");
+	} else {
+		return sessionStorage.getItem(key) && sessionStorage.getItem(key).toString().length == 0 ? '' : (sessionStorage.getItem(key) == null ? '' : sessionStorage.getItem(key));
+	}
+}
+
+function form_option_selected(obj, value) {
+	for(var i=0; i<obj.options.length; i++) {
+		if(obj.options[i].value == value) {
+			obj.options[i].selected = true;
+		}
+	}
+}
+
+function switchcredit(obj, value) {
+	var creditsettings = credit[value];
+	var s = '<select name="credit' + obj + '">';
+	for(var i in creditsettings) {
+		s += '<option value="' + creditsettings[i][0] + '">' + creditsettings[i][1] + '</option>';
+	}
+	s += '</select>';
+	$(obj).innerHTML = s;
+}
+
+function setselect(selectobj, value) {
+	var len = selectobj.options.length;
+	for(i = 0;i < len;i++) {
+		if(selectobj.options[i].value == value) {
+			selectobj.options[i].selected = true;
+		}
+	}
+}
+
+function show(id, display) {
+	if(!$(id)) return false;
+	if(display == 'auto') {
+		$(id).style.display = $(id).style.display == '' ? 'none' : '';
+	} else {
+		$(id).style.display = display;
+	}
 }
